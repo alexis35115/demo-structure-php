e505115bde569d5e239a53a7b2edd52787f86697
Alexis Garon-Michaud <alexis_35115@hotmail.com>
2021-03-21 19:47:01 -0400

    ajout de l'entête, du pied de page et du fichier CSS de base

--- index.php
+++ index.php
@@ -4,11 +4,24 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/style.css">
     <title>Exemple</title>
 </head>
 <body>
-    <?php 
-        echo("Je suis dans le body de la page");
-    ?>
+    <header>
+        <h1>Cinéma du monde</h1>
+        <nav>
+            <div style="float: right;">
+                <a href="index.php">Accueil</a>	|
+                <a href="https://www.cinemagaiete.com/">Cinéma Gaieté</a>
+            </div>
+        </nav>
+    </header>
+
+    <?php echo("je ne suis plus seul dans la page")?>
+
+    <footer>
+        <small>Tous droits réservés 2021 © Alexis Garon-Michaud</small>
+    </footer>
 </body>
 </html>
