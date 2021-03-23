f50f5317405bc77f76054a2a710bab1591a5c4d8
Alexis Garon-Michaud <alexis_35115@hotmail.com>
2021-03-22 20:14:58 -0400

    récupérer le film selon l'identifiant fourni en paramètre dans la page détail

--- film-detail.php
+++ film-detail.php
@@ -1,3 +1,15 @@
+<?php 
+    include "connexion.php";
+
+    $id_film = $_GET['id_film'];
+
+    $sth = $dbh->prepare("SELECT `titre`, `realisateur`, `resume`, `description`, `image` FROM `film` WHERE `id_film` = :id_film;");
+    $sth->bindParam(':id_film', $id_film, PDO::PARAM_INT);
+
+    $sth->execute();
+
+    $film = $sth->fetch();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,14 +17,24 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles/style.css">
-    <title>Détail d'un film</title>
+    <title><?=$film['titre']?></title>
 </head>
 <body>
     <?php 
     include "en-tete.php";
+    ?>
 
-    echo($_GET['id_film']);
+    <section>
+        <div class="espacement">
+            <h4></h4>
+            <span></span>
+            <p></p>
+            <p></p>
+            <img src="" alt="">
+        </div>
+    </section>
 
+    <?php
     include "pied-page.php";
     ?>  
 </body>
