65c49d993f689acbcf94a26a620a03473366a07f
Alexis Garon-Michaud <alexis_35115@hotmail.com>
2021-03-22 20:02:53 -0400

    création du template de base de la page détail

--- film-detail.php
+++ film-detail.php
@@ -1,3 +1,19 @@
-<?php 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/style.css">
+    <title>Détail d'un film</title>
+</head>
+<body>
+    <?php 
+    include "en-tete.php";
+
     echo($_GET['id_film']);
-?>
+
+    include "pied-page.php";
+    ?>  
+</body>
+</html>
