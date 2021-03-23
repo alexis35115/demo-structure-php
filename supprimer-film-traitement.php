cde21449d54d4bcadcbb158d2116e4ea809a0f23
Alexis Garon-Michaud <alexis_35115@hotmail.com>
2021-03-22 20:35:27 -0400

    ajout du template de base dans la page pour la suppression d'un film

--- supprimer-film-traitement.php
+++ supprimer-film-traitement.php
@@ -1,4 +1,16 @@
-<?php 
-
-    echo("grosse toutoune");
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/style.css">
+    <title>Suppression d'un film</title>
+</head>
+<body>
+    <?php 
+    include "en-tete.php";
+    include "pied-page.php";
+    ?>
+</body>
+</html>
