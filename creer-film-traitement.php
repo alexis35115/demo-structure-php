60244438e4115d592d66bd0f83e6e0ff602ccecc
Alexis Garon-Michaud <alexis_35115@hotmail.com>
2021-03-25 19:56:39 -0400

    html de base de la page de confirmation de la création d'un film

--- creer-film-traitement.php
+++ creer-film-traitement.php
@@ -1,5 +1,28 @@
-<?php 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/style.css">
+    <title>Page de confirmation de la création d'un film</title>
+</head>
+<body>
+    <?php 
+    include "en-tete.php";
+    ?>
 
-print_r($_POST);
+    <?php 
 
-?>
+    print_r($_POST);
+
+    //INSERT INTO `film`(`titre`, `resume`, `description`, `realisateur`, `image`) VALUES ([value-2],[value-3],[value-4],[value-5],[value-6])
+
+    ?>
+
+
+    <?php 
+    include "pied-page.php";
+    ?>
+</body>
+</html>
