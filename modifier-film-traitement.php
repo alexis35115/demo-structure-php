f8710fda3a294f6927932121d658785ab306f195
Alexis Garon-Michaud <alexis_35115@hotmail.com>
2021-03-25 20:30:26 -0400

    création de base des fichiers pour la modifcation d'un film

---
+++ modifier-film-traitement.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/style.css">
+    <title>Page de confirmation de la modification d'un film</title>
+</head>
+<body>
+    <?php 
+    include "en-tete.php";
+    include "pied-page.php";
+    ?>
+</body>
+</html>
